c9ce7947569fb0495978a14a13fe37de18f7e99d
Bruno Fernandes Valero <brunofvn6@gmail.com>
2025-10-12 22:49:54 -0300

    revised norma && valgrind-errors

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: valero <valero@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 17:52:30 by brunofer          #+#    #+#             */
-/*   Updated: 2025/10/09 15:36:16 by valero           ###   ########.fr       */
+/*   Updated: 2025/10/12 22:44:34 by valero           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,5 @@
 # include "viewer.h"
 # include "parse_file.h"
 # include "core.h"
-
-// typedef struct s_layers
-// {
-// 	t_minilib_layer	fdf;
-// 	t_minilib_layer	background;
-// 	t_minilib_layer	info;
-// }	t_layers;
-
-// typedef struct s_fdf
-// {
-// 	t_layers	layers;
-// }	t_fdf;
 
 #endif
